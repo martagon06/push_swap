bdc68e75a755e3128d1c1fab84516bc531f1e78c
martagon06 <miguelmorenomartagon@gmail.com>
2025-07-30 14:54:11 +0200

    .

--- rotate.c
+++ rotate.c
@@ -1,37 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push.c                                             :+:      :+:    :+:   */
+/*   rotate.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: miguelmo <miguelmo@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/26 16:57:31 by miguelmo          #+#    #+#             */
-/*   Updated: 2025/07/30 14:54:00 by miguelmo         ###   ########.fr       */
+/*   Created: 2025/07/30 14:30:52 by miguelmo          #+#    #+#             */
+/*   Updated: 2025/07/30 14:53:59 by miguelmo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void push(t_stack **src, t_stack **dest)
+void rotate(t_stack **src)
 {
-    t_node *temporal;
-
-    if (*src == NULL || !(*src)->top)
-        return;
-    temporal = (*src)->top;
-    *src = (*src)->top;
-    temporal->next = *dest;
-    *dest = temporal;
-}
-
-void    pa(t_stack **a, t_stack **b)
-{
-    push(b, a);
-    write(1, "pa\n", 3);
-}
-
-void    pb(t_stack **a, t_stack **b)
-{
-    push(a, b);
-    write(1, "pb\n", 3);
+    
 }
--- swap.c
+++ swap.c
@@ -6,11 +6,30 @@
 /*   By: miguelmo <miguelmo@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/26 20:14:00 by miguelmo          #+#    #+#             */
-/*   Updated: 2025/07/26 20:19:15 by miguelmo         ###   ########.fr       */
+/*   Updated: 2025/07/30 14:53:56 by miguelmo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void swap(t_stack *stack)
+{
+    t_node *first;
+    t_node *second;
+    if (!stack || stack->size < 2)
+        return;
+    first = stack->top;
+    second = first->next;
+    first->next = second->next;
+    if (second->next)
+        second->next->prev = first;
+    second->prev = NULL;
+    second->next = first;
+    first->prev = second;
+    stack->top = second;
+    if (stack->size == 2)
+        stack->bottom = first;
+}
 
 void sa(t_stack **a)
 {
