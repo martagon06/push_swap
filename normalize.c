bbfe39190220a28cdd6a98a099446e7cb3840b95
miguelmo <miguelmorenomartagon@gmail.com>
2025-09-05 13:05:29 +0200

    solo quedan utils3, y calculate_cost, mucho cuidado con los costes

--- normalize.c
+++ normalize.c
@@ -6,7 +6,7 @@
 /*   By: miguelmo <miguelmo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/08 18:08:51 by miguelmo          #+#    #+#             */
-/*   Updated: 2025/09/04 19:44:27 by miguelmo         ###   ########.fr       */
+/*   Updated: 2025/09/05 13:03:38 by miguelmo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,27 +34,27 @@ static void	assign_indexes(t_stack *a, int *values, int size)
 	}
 }
 
-void	normalize(t_stack *a)
+static void	fill_values_array(t_stack *a, int *values, int size)
 {
-	int		size;
-	int		i;
-	int		j;
-	int		tmp;
-	int		*values;
 	t_node	*cur;
+	int		i;
 
-	i = 0;
-	size = a->size;
-	values = malloc(sizeof(int) * size);
-	if (!values)
-		return ;
 	cur = a->top;
+	i = 0;
 	while (i < size)
 	{
 		values[i] = cur->value;
 		cur = cur->next;
 		i++;
 	}
+}
+
+static void	sort_values(int *values, int size)
+{
+	int	i;
+	int	j;
+	int	tmp;
+
 	i = 0;
 	while (i < size - 1)
 	{
@@ -71,6 +71,19 @@ void	normalize(t_stack *a)
 		}
 		i++;
 	}
+}
+
+void	normalize(t_stack *a)
+{
+	int		size;
+	int		*values;
+
+	size = a->size;
+	values = malloc(sizeof(int) * size);
+	if (!values)
+		return ;
+	fill_values_array(a, values, size);
+	sort_values(values, size);
 	assign_indexes(a, values, size);
 	free(values);
 }
